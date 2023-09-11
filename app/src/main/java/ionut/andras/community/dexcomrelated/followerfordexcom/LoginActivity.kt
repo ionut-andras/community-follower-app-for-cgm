84d132ff828e6c463ecb026df8bee5b8b005f9bc
Ionut Andras <ionut.andras@gmail.com>
2023-09-11 18:44:28 +0300

    + Code redesign

--- app/src/main/java/ionut/andras/community/dexcomrelated/followerfordexcom/LoginActivity.kt
+++ app/src/main/java/ionut/andras/community/dexcomrelated/followerfordexcom/LoginActivity.kt
@@ -2,7 +2,6 @@ package ionut.andras.community.dexcomrelated.followerfordexcom
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
@@ -32,7 +31,6 @@ class LoginActivity : AppCompatActivityWrapper() {
     fun checkboxDisclaimerChanged(view: View) {
         val disclaimerCheckbox = findViewById<CheckBox>(R.id.checkboxDisclaimer)
         val loginButton = findViewById<Button>(R.id.btnLogin)
-Log.i("checkboxDisclaimerChanged: ", disclaimerCheckbox.isChecked.toString())
         loginButton.isEnabled = disclaimerCheckbox.isChecked
     }
 
