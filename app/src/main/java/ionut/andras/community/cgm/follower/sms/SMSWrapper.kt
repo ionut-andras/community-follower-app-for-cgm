98877d7909e20a25506c752b07edb3ffe23977ec
ionut-andras <34043845+ionut-andras@users.noreply.github.com>
2023-10-16 00:31:24 +0300

    + Updating Invite Follower to get an user key from the Back-End + Adding support to open application directly form an SMS + Fixing but of adding multiple times the same phone number in the list of memorized follower numbers

--- app/src/main/java/ionut/andras/community/cgm/follower/sms/SMSWrapper.kt
+++ app/src/main/java/ionut/andras/community/cgm/follower/sms/SMSWrapper.kt
@@ -3,62 +3,18 @@ package ionut.andras.community.cgm.follower.sms
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
-import android.telephony.SmsManager
-import android.telephony.TelephonyManager
 
 
 class SMSWrapper(private val applicationContext: Context) {
     fun sendTextSms(phoneNumber: String, message: String) {
         // Create an Intent to start the default SMS app.
-        // Create an Intent to start the default SMS app.
-        val intent = Intent(Intent.ACTION_SENDTO).apply {
-            setDataAndType(Uri.parse("sms:$phoneNumber"), "text/plain")
-            putExtra("sms_body", message)
-        }
         // Set the phone number and message text.
-        if (intent.resolveActivity(applicationContext.packageManager) != null) {
-            // Start the default SMS app.
-            applicationContext.startActivity(intent)
-            // startActivity(applicationContext, intent, null)
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            data = Uri.parse("sms:$phoneNumber")
+            putExtra(Intent.EXTRA_TEXT, message)
         }
-    }
-
-    fun sendTextSmsOrig(phoneNumber: String, message: String) {
-        val telephonyManager = applicationContext.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-
-        if (telephonyManager.simState != TelephonyManager.SIM_STATE_READY) {
-            // Device cannot send SMS messages
-            return
-        }
-
-        val smsManager: SmsManager = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            applicationContext.getSystemService(SmsManager::class.java)
-        } else {
-            SmsManager.getDefault()
-        }
-
-        /*
-        val binaryMessage = message.toByteArray(Charsets.UTF_8)
-
-        smsManager.sendDataMessage(
-            phoneNumber,
-            null,  // Service Center Address (use null for default)
-            0,     // Port (use 0 for default)
-            binaryMessage,
-            null,
-            null
-        )
-        */
-
-        smsManager.sendTextMessage(
-            phoneNumber,
-            null,
-            message,
-            null,
-            null
-        )
-
-        return
+       // Start the default SMS app.
+        applicationContext.startActivity(intent)
     }
 }
