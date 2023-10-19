1bd00590448041f13070d61d688714a5f273b5a5
ionut-andras <34043845+ionut-andras@users.noreply.github.com>
2023-10-20 00:37:13 +0300

    + Cleanup process of login + Improved login process + Adding more logs

---
+++ app/src/main/java/ionut/andras/community/cgm/follower/LoadingAuthenticationActivity.kt
@@ -0,0 +1,11 @@
+package ionut.andras.community.cgm.follower
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadingAuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading_authentication)
+    }
+}
